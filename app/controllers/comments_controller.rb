931c955101a2da347737274ff75a3e29e61299f8
Arshdeep Singh & Allain Dollete <pair+arsy209.jdollete@devbootcamp.com>
2016-12-05 18:02:58 -0500

    Save point

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,21 @@
 class CommentsController < ApplicationController
 
-
 def new
+  @comment = Comment.new
 end
+
+def create
+  @post = Post.find(params[:post_id])
+  @comment = Comment.new(comment_params)
+  @comment.user_id = current_user.id
+  @comment.post_id = params[:post_id]
+   if @comment.save
+  redirect_to @post
+end
+end
+
+private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
